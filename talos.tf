7e5c9df918415348d76a90a4e0305d4d3915d45a
Stefan Le Breton <who@cares.net>
2024-11-22 14:22:12 +0100

    fixed deprecated data for kubeconfig to resource

--- talos.tf
+++ talos.tf
@@ -160,7 +160,8 @@ resource "talos_machine_configuration_apply" "worker" {
   node                        = each.value.ipv4_address
 }
 
-data "talos_cluster_kubeconfig" "this" {
+resource "talos_cluster_kubeconfig" "this" {
+  depends_on = [ talos_machine_bootstrap.this ]
   client_configuration = talos_machine_secrets.this.client_configuration
   node                 = local.endpoint_ip
   #  wait                 = true
