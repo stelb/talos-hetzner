196b06dc01944b81bdcee4d7700f8ec464dcd1ba
Stefan Le Breton <who@cares.net>
2024-11-23 19:44:26 +0100

    reformat

--- talos.tf
+++ talos.tf
@@ -161,7 +161,7 @@ resource "talos_machine_configuration_apply" "worker" {
 }
 
 resource "talos_cluster_kubeconfig" "this" {
-  depends_on = [ talos_machine_bootstrap.this ]
+  depends_on           = [talos_machine_bootstrap.this]
   client_configuration = talos_machine_secrets.this.client_configuration
   node                 = local.endpoint_ip
   #  wait                 = true
